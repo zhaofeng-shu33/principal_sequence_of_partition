1c5e9a7446ccc51289c956adb144b47f8a7748e1
zhaofeng-shu33 <616545598@qq.com>
2018-10-31 21:35:36 +0800

    psp of hypergraphicalmodel with 6 elements, test passed

--- test/test_dt.cpp
+++ test/test_dt.cpp
@@ -48,6 +48,38 @@ namespace submodular {
                 return 2;
         }
     };
+    template <typename ValueType>
+    class HyperGraphicalModel2 : public SubmodularOracle<ValueType> {
+    public:
+        std::string GetName() { return ""; }
+        HyperGraphicalModel2()
+        {
+            this->SetDomain(Set::MakeDense(6));
+        }
+        value_type Call(const Set& X) {
+            auto members = X.GetMembers();
+            bool edges[4] = { 0,0,0,0 };
+            for (const auto i : members) {
+                if (i == 0 || i == 1) {
+                    edges[0] = 1;
+                    edges[3] = 1;
+                }
+                else if (i == 2) {
+                    edges[0] = 1;
+                }
+                else if (i == 3 || i == 4) {
+                    edges[1] = 1;
+                }
+                else // i == 5
+                    edges[2] = 1;
+            }
+            int sum = 0;
+            for (const auto i : edges)
+                sum += i;
+            return sum;
+        }
+
+    };
     TEST(DilworthTruncation, SampleFunction){
         DilworthTruncation<float> dt(&SampleFunction<float>(), 2);
         dt.Run();
@@ -99,4 +131,64 @@ namespace submodular {
         // partition list is {{{0, 1, 2}}, {{0, 1}, {2}}, {{0}, {1}, {2}}}  
         // gamma list is {1, 2}
     }
+    TEST(DilworthTruncation, HyperGraphicalModel2) {
+        DilworthTruncation<float> dt(&HyperGraphicalModel2<float>(), 1.5);
+        dt.Run();
+        EXPECT_EQ(dt.Get_min_value(), -1.5);
+        std::vector<Set> p = dt.Get_min_partition();
+        EXPECT_EQ(p.size(), 5);
+        EXPECT_EQ(p[0], Set::FromIndices(6, { 0, 1 }));
+        EXPECT_EQ(p[1], Set::FromIndices(6, { 2 }));
+        EXPECT_EQ(p[2], Set::FromIndices(6, { 3 }));
+        EXPECT_EQ(p[3], Set::FromIndices(6, { 4 }));
+        EXPECT_EQ(p[4], Set::FromIndices(6, { 5 }));
+        // optimal value is -1.5
+        // optimal partition is{ 0, 1 }, { 2 }, { 3 }, { 4 }, { 5 }  
+    }
+    TEST(PSP, HyperGraphicalModel2) {
+        HyperGraphicalModel2<float>* hgm = new HyperGraphicalModel2<float>();
+        PSP<float> psp_class(hgm);
+        psp_class.run();
+        std::vector<float> gamma_list = psp_class.Get_critical_values();
+        std::vector<std::vector<Set>> psp_list = psp_class.Get_psp();
+        EXPECT_EQ(gamma_list.size(), 6);
+        // effective element of gamma_list is corresponding to non-empty element of psp_list
+        EXPECT_EQ(gamma_list[0], 0);
+        EXPECT_EQ(gamma_list[2], 1);
+        EXPECT_EQ(gamma_list[4], 2);
+
+        EXPECT_EQ(psp_list.size(), 6);
+
+        EXPECT_EQ(psp_list[0].size(), 1);
+        EXPECT_EQ(psp_list[0][0], Set::MakeDense(6));
+
+        EXPECT_EQ(psp_list[1].size(), 0);
+
+        EXPECT_EQ(psp_list[2].size(), 3);
+        EXPECT_EQ(psp_list[2][0], Set::FromIndices(6, { 0, 1, 2 }));
+        EXPECT_EQ(psp_list[2][1], Set::FromIndices(6, { 3, 4 }));
+        EXPECT_EQ(psp_list[2][2], Set::FromIndices(6, { 5 }));
+
+        EXPECT_EQ(psp_list[3].size(), 0);
+
+        EXPECT_EQ(psp_list[4].size(), 5);
+        EXPECT_EQ(psp_list[4][0], Set::FromIndices(6, { 0, 1 }));
+        EXPECT_EQ(psp_list[4][1], Set::FromIndices(6, { 2 }));
+        EXPECT_EQ(psp_list[4][2], Set::FromIndices(6, { 3 }));
+        EXPECT_EQ(psp_list[4][3], Set::FromIndices(6, { 4 }));
+        EXPECT_EQ(psp_list[4][4], Set::FromIndices(6, { 5 }));
+
+        EXPECT_EQ(psp_list[5].size(), 6);
+        EXPECT_EQ(psp_list[5][0], Set::FromIndices(6, { 0 }));
+        EXPECT_EQ(psp_list[5][1], Set::FromIndices(6, { 1 }));
+        EXPECT_EQ(psp_list[5][2], Set::FromIndices(6, { 2 }));
+        EXPECT_EQ(psp_list[5][3], Set::FromIndices(6, { 3 }));
+        EXPECT_EQ(psp_list[5][4], Set::FromIndices(6, { 4 }));
+        EXPECT_EQ(psp_list[5][5], Set::FromIndices(6, { 5 }));
+
+        delete hgm;
+        // partition list is {{{0, 1, 2}}, {{0, 1}, {2}}, {{0}, {1}, {2}}}  
+        // gamma list is {1, 2}
+    }
+
 }
