b9dfde9ef69359a5d3b631439d555c7ee62767e2
zhaofeng-shu33 <616545598@qq.com>
2018-11-06 23:40:03 +0800

    support for python build, both debug and release

--- core/python/psp.cpp
+++ core/python/psp.cpp
@@ -1,1 +1,8 @@
-#include <Python.h>
+#include <boost/python.hpp>
+#include "config.h"
+using namespace boost::python;
+BOOST_PYTHON_MODULE(psp)
+{
+    scope().attr("__version__") = PSP_VERSION_MAJOR;
+
+}
