f0e04409969a4a3aa83e343758a89940f647b0ea
zhaofeng-shu33 <616545598@qq.com>
2018-11-21 10:41:55 +0800

    update gtest build, install

--- utility/gtest.sh
+++ utility/gtest.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # build and install gtest
 pushd /usr/src/gtest
-cmake .
-make install
+cmake ./
+make
+# install by symobolic linking
+sudo ln -s -t /usr/lib  /usr/src/gtest/libgtest.a /usr/src/gtest/libgtest_main.a
 popd
