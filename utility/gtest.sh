7ae6cc71491a5f30dd831847fafba434287c4db1
zhaofeng-shu33 <616545598@qq.com>
2018-11-21 15:54:32 +0800

    line split

--- utility/gtest.sh
+++ utility/gtest.sh
@@ -4,5 +4,6 @@ pushd /usr/src/gtest
 cmake ./
 make
 # install by symobolic linking
-sudo ln -s -t /usr/lib  /usr/src/gtest/libgtest.a /usr/src/gtest/libgtest_main.a
+sudo ln -s -t /usr/lib /usr/src/gtest/libgtest.a
+sudo ln -s -t /usr/lib /usr/src/gtest/libgtest_main.a
 popd
