f6df58a557c5f4f225d07ed74a0b0c530e8ef7de
zhaofeng-shu33 <616545598@qq.com>
2018-11-22 10:22:53 +0800

    remote sh file

--- utility/gtest.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# build and install gtest
-make
-# install by symobolic linking
-sudo ln -s -t /usr/lib /usr/src/gtest/libgtest.a
-sudo ln -s -t /usr/lib /usr/src/gtest/libgtest_main.a
-
