4d20fc60a9af88c740d02b256bf3a28ce7d5b7be
zhaofeng-shu33 <616545598@qq.com>
2018-11-22 10:36:14 +0800

    not find eigen3 config directory

--- core/python/psp.cpp
+++ core/python/psp.cpp
@@ -7,5 +7,5 @@ BOOST_PYTHON_MODULE(psp)
     scope().attr("__version__") = PSP_VERSION_MAJOR;
     class_<demo::Gaussian2DGraph>("Gaussian2DGraph", init<float>())
         .def("run", &demo::Gaussian2DGraph::run)
-        .def("get_gamma_list", &demo::Gaussian2DGraph::get_gamma_list);
+        .def("get_gamma_list", &demo::Gaussian2DGraph::get_gamma_list, return_internal_reference<>());
 }
