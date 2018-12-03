e377a1d14c31b0284663affc44ca06778c8ea100
zhaofeng-shu33 <616545598@qq.com>
2018-12-03 11:40:23 +0800

    double quote around env variable in cmake

--- utility/graph_cut_py_test.py
+++ utility/graph_cut_py_test.py
@@ -48,6 +48,23 @@ class TestPyGraph(unittest.TestCase):
         self.assertEqual(cat_2_list, g2.get_category(2))
         self.assertEqual(cv_list, g2.critical_values)
         self.assertEqual(p_list, g2.partation_num_list)
-        
+
+    def test_8point(self):
+        gamma = 0.6
+        num_points = 8
+        g = psp.Gaussian2DGraph(num_points,gamma)
+        pos_list = construct_pos_list(g.get_x_pos_list(), g.get_y_pos_list())
+        # Method 1
+        g.run(False)
+        cv_list = to_py_list(g.get_critical_values())
+        p_list = to_py_list(g.get_partations())
+        cat_2_list = to_py_list(g.get_category(4))
+        # Method 2
+        g2 = gaussian2Dcase(num_points, gamma, pos_list)
+        g2.run()
+        # assert
+        self.assertEqual(cat_2_list, g2.get_category(4))
+        self.assertEqual(cv_list, g2.critical_values)
+        self.assertEqual(p_list, g2.partation_num_list)        
 if __name__ == '__main__':
     unittest.main()
