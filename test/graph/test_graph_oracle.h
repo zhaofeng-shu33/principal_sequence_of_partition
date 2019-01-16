4d6fc03fa05576b9d37e319a9b50e2a8fcd26055
zhaofeng-shu33 <616545598@qq.com>
2019-01-16 14:42:52 +0800

    pin model test

--- test/graph/test_graph_oracle.h
+++ test/graph/test_graph_oracle.h
@@ -7,26 +7,7 @@
 #include "core/dt.h"
 namespace submodular {
 
-    class PINModelTest : public testing::Test {
-    protected:
-        using EdgeListFloat = std::vector<std::tuple<std::size_t, std::size_t, float>>;
 
-        // {src, dst, capacity}
-        std::size_t n_1 = 3;
-        std::size_t data_1[3][3] = {
-        { 0, 1, 1 },
-        { 0, 2, 5 },
-        { 1, 2, 1 },
-        };
-
-        EdgeListFloat edge_list_float_1;
-
-        virtual void SetUp() {
-            for (const auto& sdc : data_1) {
-                edge_list_float_1.push_back(std::make_tuple(sdc[0], sdc[1], (float)sdc[2]));
-            }
-        }
-    };
     TEST_F(MaxflowGraphTest, GraphOracleBase) {
         MaxflowGraph<int> graph_int_1 = st_graph(n_1, s_1, t_1, edge_list_int_1);
         GeneralizedCutOracle<int> gc;
