29f13c2ee3fcd4634ab5b6172a7ab6af86b3d783
zhaofeng-shu33 <616545598@qq.com>
2019-02-15 16:45:07 +0800

    init code finished

--- core/pmf.cpp
+++ core/pmf.cpp
@@ -32,6 +32,56 @@ double compute_lambda(const std::vector<pair>& parameter_list, const double targ
     }
     return (target_value - sum) / slope + last_tp;
 }
+namespace submodular{
+void PMF::init() {
+    lemon::DigraphCopy<lemon::ListDigraph, lemon::ListDigraph> cg(*g_ptr, dig);
+    cg.arcMap(*aM, dig_aM);
+    cg.run();
+    sink_node = dig.nodeFromId(dig.maxNodeId());
+    source_node = dig.addNode();
+    tilde_G_size = dig.maxNodeId() + 1;
+    for (lemon::ListDigraph::NodeIt n(dig); n != lemon::INVALID; n++) {
+        if (n == sink_node || n == source_node)
+            continue;
+        dig.addArc(source_node, n);
+        dig.addArc(n, sink_node);
+    }
+    // reset the source_node and sink_node
+    pf.source(source_node);
+    pf.target(sink_node);
+    //find S_0 and T_0
+    update_dig(0);
+    pf.init();
+    pf.startFirstPhase();
+    pf.startSecondPhase();
+    double minimum_value = pf.flowValue();
+    Set S_0 = Set::MakeEmpty(tilde_G_size);
+    for (lemon::ListDigraph::NodeIt n(dig); n != lemon::INVALID; n++) {
+        if (!pf.minCut(n))
+            S_0.AddElement(dig.id(n));
+    }
+    Set T_0 = S_0.Complement();
+    Set T_1 = Set::MakeEmpty(tilde_G_size);
+    T_1.AddElement(tilde_G_size - 2);
+    set_list.push_back(T_0);
+    set_list.push_back(T_1);
+}
+void PMF::update_dig(double lambda) {
+    for (lemon::ListDigraph::OutArcIt arc(dig, source_node); arc != lemon::INVALID; arc++) {
+        // get the next node id
+        int i = dig.id(dig.target(arc));
+        double a_i = _y_lambda[i].first, b_i = _y_lambda[i].second;
+        dig_aM[arc] = std::max<double>(0, -std::min<double>(a_i - 2 * lambda, b_i));
+    }
+    for (lemon::ListDigraph::InArcIt arc(dig, sink_node); arc != lemon::INVALID; arc++) {
+        int i = dig.id(dig.source(arc));
+        double a_i = _y_lambda[i].first, b_i = _y_lambda[i].second;
+        // get the arc from the original graph
+        lemon::ListDigraph::Arc o_arc = g_ptr->arcFromId(dig.id(arc));
+        dig_aM[arc] = aM->operator[](o_arc) + std::max<double>(0, std::min<double>(a_i - 2 * lambda, b_i));
+    }
+}
+}
 int remain(){
     // set elevator and flow maps before running the algorithm.
     lemon::ListDigraph g;
--- core/pmf.h
+++ core/pmf.h
@@ -1,8 +1,37 @@
 #pragma once
 #include <tuple>
 #include <numeric>
+#include <memory>
 #include <lemon/list_graph.h>
 #include "core/parametric_preflow.h"
+#include "core/set_utils.h"
 #include <iostream>
 typedef std::pair<double, double> pair;
 double compute_lambda(const std::vector<pair>& parameter_list, const double target_value);
+namespace submodular{
+class PMF {
+public:
+    typedef lemon::ListDigraph::ArcMap<double> ArcMap;
+    PMF(lemon::ListDigraph& g, ArcMap& arcMap, std::vector<pair>& y_lambda):
+        g_ptr(&g), aM(&arcMap),
+        _y_lambda(y_lambda),
+        dig_aM(dig),
+        pf(dig, dig_aM, lemon::INVALID, lemon::INVALID){}
+    void init();
+    void run();
+private:
+    void update_dig(double lambda);
+    void slice(Set S, Set T);
+    std::unique_ptr<lemon::ListDigraph> g_ptr;
+    std::unique_ptr<ArcMap> aM;
+    lemon::ListDigraph dig;
+    ArcMap dig_aM;
+    std::vector<pair> _y_lambda;
+    std::list<double> lambda_list;
+    std::list<Set> set_list;
+    lemon::ListDigraph::Node source_node;
+    lemon::ListDigraph::Node sink_node;
+    int tilde_G_size;
+    lemon::Preflow<lemon::ListDigraph, ArcMap> pf;
+};
+}
