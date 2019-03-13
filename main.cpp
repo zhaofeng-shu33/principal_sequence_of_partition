6cac5d120ec40e07424d532e7974f673e19e846a
zhaofeng-shu33 <616545598@qq.com>
2019-03-13 16:05:38 +0800

    prepare to add pdt support to main.exe

--- main.cpp
+++ main.cpp
@@ -14,6 +14,12 @@ void check_size(int size) {
         exit(0);
     }
 }
+void check_positive(double gamma) {
+    if (gamma <= 0) {
+        std::cout << "gamma must be positive number";
+        exit(0);
+    }
+}
 //! To be finished
 int main(int argc, const char *argv[]){
 #if USE_BOOST
@@ -21,6 +27,8 @@ int main(int argc, const char *argv[]){
     desc.add_options()
         ("help,h", "Show this help screen")
         ("full", boost::program_options::value<bool>()->default_value(true))
+        ("pdt", boost::program_options::value<bool>()->default_value(false), "whether to use parametric Dilworth truncation")
+        ("gamma", boost::program_options::value<double>()->default_value(0.5)->notifier(check_positive))
         ("size", boost::program_options::value<int>()->default_value(8)->notifier(check_size), "total number of points to be classified, must be multiplier of 4");
     boost::program_options::variables_map vm;
     boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
@@ -29,20 +37,30 @@ int main(int argc, const char *argv[]){
         std::cout << desc << '\n';
         return 0;
     }
-    submodular::InfoCluster* g2g = new demo::Gaussian2DGraph(vm["size"].as<int>(),0.5);
-#else
-    demo::Gaussian2DGraph<double>* g2g = new demo::Gaussian2DGraph<double>(8);
 #endif
+    int size = vm["size"].as<int>();
     bool run_full = vm["full"].as<bool>();
-    std::vector<submodular::Set> p;
-    if(run_full){
-        g2g->run();
-        p = g2g->get_smallest_partition(4);
+    bool use_pdt = vm["pdt"].as<bool>();
+    bool _gamma = vm["gamma"].as<double>();
+    if (use_pdt) {
+
     }
     else {
-        p = g2g->get_partition(4);
+#if USE_BOOST
+        submodular::InfoCluster* g2g = new demo::Gaussian2DGraph(size, _gamma);
+#else
+        demo::Gaussian2DGraph<double>* g2g = new demo::Gaussian2DGraph<double>(8);
+#endif
+        std::vector<submodular::Set> p;
+        if (run_full) {
+            g2g->run();
+            p = g2g->get_smallest_partition(4);
+        }
+        else {
+            p = g2g->get_partition(4);
+        }
+        std::cout << p;
     }
-    std::cout << p;
     delete g2g;
     return 0;
 }
