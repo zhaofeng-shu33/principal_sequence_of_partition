e97f4fcc3e4c22c819ee143784f373b936c27283
zhaofeng-shu33 <616545598@qq.com>
2019-03-27 15:52:32 +0800

    update info_cluster python code

--- utility/info_cluster.py
+++ utility/info_cluster.py
@@ -1,10 +1,8 @@
-import random
-import math
-import cmath
 import numpy as np
-import psp # [package] principal sequence of partition
 from sklearn.metrics.pairwise import pairwise_kernels
 from sklearn.neighbors import kneighbors_graph
+
+import psp # [package] principal sequence of partition
 
 class InfoCluster:
     '''Info clustering is a kind of hierarchical clustering method.
@@ -27,7 +25,7 @@ class InfoCluster:
         self.affinity = affinity
         self.n_neighbors = n_neighbors     
         self.n_clusters = n_clusters
-    def fit(self, X):
+    def fit(self, X, use_pdt = False):
         '''Construct an affinity graph from X using rbf kernel function,
         then applies info clustering to this affinity graph.
         Parameters
@@ -35,9 +33,9 @@ class InfoCluster:
         X : array-like, shape (n_samples, n_features)
            
         '''
-        if(self.n_clusters is not None):
+        if(self.n_clusters is not None and use_pdt == False):
             return self.get_category(self.n_clusters, X)
-        self._init_g(X)
+        self._init_g(X, use_pdt = False)
         self.g.run(False)
         
         self.critical_values = to_py_list(self.g.get_critical_values())
@@ -100,7 +98,7 @@ class InfoCluster:
                 return i
         return -1       
         
-    def _init_g(self, X):
+    def _init_g(self, X, use_pdt = False):
         n_samples = X.shape[0]
         if(self.affinity == 'precomputed'):
             affinity_matrix = X
@@ -114,74 +112,11 @@ class InfoCluster:
         for s_i in range(n_samples):
             for s_j in range(s_i+1, n_samples):
                 sim_list.append((s_i, s_j, affinity_matrix[s_i, s_j]))       
+        if(use_pdt):
+            self.g = psp.PyGraphPDT(n_samples, sim_list)
+        else:
+            self.g = psp.PyGraph(n_samples, sim_list)    
 
-        self.g = psp.PyGraph(n_samples, sim_list)    
-class ThreeCircle:
-    def __init__(self, np_list, gamma_1=1, gamma_2=1):
-        '''
-        np is the number of points at each circle
-        '''
-        #  (0, 0.1) to (0, 0.2)
-        pos_list = []
-        self._np = 10;
-        for i in range(10):
-            pos_list.append([0, random.random()*0.1+0.1])
-        
-        for i in range(1,4): # radius: 0.1*i
-            self._np += np_list[i-1]
-            for j in range(np_list[i-1]):
-                r = 0.1*i
-                angle = 2*math.pi * random.random()
-                pos_list.append([r * math.cos(angle), r * math.sin(angle)])
-                
-        self._gamma_1 = gamma_1;
-        self._gamma_2 = gamma_2;
-        self.affinity_matrix = np.zeros([self._np, self._np])
-        self.pos_list = pos_list
-        for s_i in range(len(pos_list)):
-            for s_j in range(s_i+1,len(pos_list)):
-                x_1,y_1 = pos_list[s_i]
-                x_2,y_2 = pos_list[s_j]
-                r_1, phi_1 = cmath.polar(complex(x_1, y_1))
-                r_2, phi_2 = cmath.polar(complex(x_2, y_2))                
-                self.affinity_matrix[s_i, s_j] = self.compute_similarity(r_1, phi_1, r_2, phi_2)
-                self.affinity_matrix[s_j, s_i] = self.affinity_matrix[s_i, s_j]
-                
-    def compute_similarity(self, r_1, phi_1, r_2, phi_2):
-        phi_distance = min(abs(phi_1 - phi_2), 2*math.pi-abs(phi_1 - phi_2))
-        return math.exp(-1.0 * self._gamma_1 * math.pow(r_1 - r_2, 2) - self._gamma_2 * math.pow(phi_distance, 2))
-        
-    def run(self):
-        g = InfoCluster(affinity = 'precomputed')
-        g.fit(self.affinity_matrix)
-        self.partition_num_list = g.partition_num_list
-        self.critical_values = g.critical_values    
-        self.get_category = g.get_category         
-        self.g = g
-    
-class FourPart:
-    def __init__(self, _np, _gamma=1):
-        '''
-        np is the number of points at each part
-        '''
-        #  (0, 0.1) to (0, 0.2)
-        self._gamma = _gamma
-        pos_list = []
-        part_center = [[3,3],[3,-3],[-3,-3],[-3,3]]
-        for i in range(4): # radius: 0.1*i
-            for j in range(_np):
-                x = part_center[i][0] + random.gauss(0,1) # standard normal distribution disturbance
-                y = part_center[i][1] + random.gauss(0,1)                
-                pos_list.append([x, y])
-        self.pos_list = np.asarray(pos_list)
-        
-    def run(self):
-        g = InfoCluster(gamma = self._gamma)
-        g.fit(self.pos_list)
-        self.partition_num_list = g.partition_num_list
-        self.critical_values = g.critical_values
-        self.get_category = g.get_category        
-        self.g = g
 def to_py_list(L):
     '''
     convert an iterable object (exported from C++ class) to numpy 1d array        
--- utility/plot_art.py
+++ utility/plot_art.py
@@ -3,15 +3,84 @@
     plot artificial dataset
 '''
 import os
+import random
+import math
+import cmath
 
 from matplotlib import pyplot as plt
 import numpy as np
 
-import info_cluster
+from info_cluster import InfoCluster
 import schema
 color_list = ['#3FF711', 'r', 'g', 'm','y','k','c','#00FF00']
 marker_list = ['o', 'v', 's', '*', '+', 'x', 'D', '1']
 MAX_CAT = len(color_list)
+class ThreeCircle:
+    def __init__(self, np_list, gamma_1=1, gamma_2=1):
+        '''
+        np is the number of points at each circle
+        '''
+        #  (0, 0.1) to (0, 0.2)
+        pos_list = []
+        self._np = 10;
+        for i in range(10):
+            pos_list.append([0, random.random()*0.1+0.1])
+        
+        for i in range(1,4): # radius: 0.1*i
+            self._np += np_list[i-1]
+            for j in range(np_list[i-1]):
+                r = 0.1*i
+                angle = 2*math.pi * random.random()
+                pos_list.append([r * math.cos(angle), r * math.sin(angle)])
+                
+        self._gamma_1 = gamma_1;
+        self._gamma_2 = gamma_2;
+        self.affinity_matrix = np.zeros([self._np, self._np])
+        self.pos_list = pos_list
+        for s_i in range(len(pos_list)):
+            for s_j in range(s_i+1,len(pos_list)):
+                x_1,y_1 = pos_list[s_i]
+                x_2,y_2 = pos_list[s_j]
+                r_1, phi_1 = cmath.polar(complex(x_1, y_1))
+                r_2, phi_2 = cmath.polar(complex(x_2, y_2))                
+                self.affinity_matrix[s_i, s_j] = self.compute_similarity(r_1, phi_1, r_2, phi_2)
+                self.affinity_matrix[s_j, s_i] = self.affinity_matrix[s_i, s_j]
+                
+    def compute_similarity(self, r_1, phi_1, r_2, phi_2):
+        phi_distance = min(abs(phi_1 - phi_2), 2*math.pi-abs(phi_1 - phi_2))
+        return math.exp(-1.0 * self._gamma_1 * math.pow(r_1 - r_2, 2) - self._gamma_2 * math.pow(phi_distance, 2))
+        
+    def run(self):
+        g = InfoCluster(affinity = 'precomputed')
+        g.fit(self.affinity_matrix)
+        self.partition_num_list = g.partition_num_list
+        self.critical_values = g.critical_values    
+        self.get_category = g.get_category         
+        self.g = g
+    
+class FourPart:
+    def __init__(self, _np, _gamma=1):
+        '''
+        np is the number of points at each part
+        '''
+        #  (0, 0.1) to (0, 0.2)
+        self._gamma = _gamma
+        pos_list = []
+        part_center = [[3,3],[3,-3],[-3,-3],[-3,3]]
+        for i in range(4): # radius: 0.1*i
+            for j in range(_np):
+                x = part_center[i][0] + random.gauss(0,1) # standard normal distribution disturbance
+                y = part_center[i][1] + random.gauss(0,1)                
+                pos_list.append([x, y])
+        self.pos_list = np.asarray(pos_list)
+        
+    def run(self):
+        g = InfoCluster(gamma = self._gamma)
+        g.fit(self.pos_list)
+        self.partition_num_list = g.partition_num_list
+        self.critical_values = g.critical_values
+        self.get_category = g.get_category        
+        self.g = g
 def check_cat(min_num, partition):
     '''
     return the index of partition whose first element is no smaller than min_num,
@@ -67,7 +136,7 @@ def plot_FourPart():
     global color_list, marker_list, MAX_CAT
     i = -1
     while(i < 0): # check category requirement, regenerate the points if necessary
-        g = info_cluster.FourPart(25, 0.6)
+        g = FourPart(25, 0.6)
         g.run()    
         # divide into >=4 parts        
         i = check_cat(4, g.partition_num_list)
@@ -80,7 +149,7 @@ def plot_ThreeCircle():
     while(i < 0): # check category requirement, regenerate the points if necessary
         gamma_2 = 1
         ratio = 2000
-        g = info_cluster.ThreeCircle([60,100,140], ratio*gamma_2, gamma_2)
+        g = ThreeCircle([60,100,140], ratio*gamma_2, gamma_2)
         g.run()    
         # divide into >=4 parts        
         i = check_cat(2, g.partition_num_list)
