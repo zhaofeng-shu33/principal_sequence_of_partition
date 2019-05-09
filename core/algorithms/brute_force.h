3285c349a899db8d3d13d7a67e63fcac718d23c9
zhaofeng-shu33 <616545598@qq.com>
2019-05-10 00:47:09 +0800

    too late tonight.

--- core/algorithms/brute_force.h
+++ core/algorithms/brute_force.h
@@ -17,6 +17,8 @@
 #include "core/oracle.h"
 #include "core/set_utils.h"
 #include "core/sfm_algorithm.h"
+#include "core/graph.h"
+#include <lemon/list_graph.h>
 
 namespace submodular {
 
@@ -30,8 +32,35 @@ public:
   std::string GetName() { return "Brute Force"; }
 
   void Minimize(SubmodularOracle<ValueType>& F);
-
+  void Minimize(SubmodularOracle<ValueType>* sf, std::vector<ValueType>& xl, ValueType lambda_, lemon::ListGraph* _g, lemon::ListGraph::EdgeMap<ValueType>* _edge_map);
 };
+
+template <typename ValueType>
+void Minimize(SubmodularOracle<ValueType>* sf, std::vector<ValueType>& xl, ValueType lambda_, lemon::ListGraph* _g, lemon::ListGraph::EdgeMap<ValueType>* _edge_map) {
+	int n_ground = xl.size();
+	stl::CSet minimizer;
+
+	value_type min_value = std::numeric_limits<value_type>::max();
+	stl::CSet fixed;
+	fixed.AddElement(n_ground);
+	for (unsigned long i = 0; i < (1 << n_ground); ++i) {
+		stl::CSet X(n_ground, i);
+		// divide it by 2 !		
+		value_type new_value = get_cut_value(*_g, *_edge_map, X.Union(fixed)) /2;
+		for(int j: X)
+			new_value -= xl[j]
+		if (new_value < min_value) {
+			min_value = new_value;
+			minimizer = X;
+		}
+	}
+	min_value -= lambda_;
+
+	Set minimizer_set(n_ground);
+	for (int i : minimizer)
+		minimizer_set.AddElement(i);
+	this->SetResults(min_value, minimizer_set);
+}
 
 template <typename ValueType>
 void BruteForce<ValueType>::Minimize(SubmodularOracle<ValueType>& F) {
