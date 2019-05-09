9186f21481649631083f5f9bd06f4c8101bba019
zhaofeng-shu33 <616545598@qq.com>
2019-05-09 14:54:00 +0800

    fix eigen3

--- core/graph/info_cluster.h
+++ core/graph/info_cluster.h
@@ -64,7 +64,7 @@ namespace submodular {
 #if USE_EIGEN3
         void agglomerative_run() {
             DirectedGraphCutOracle<double>* dgc = new DirectedGraphCutOracle<double>(sg);
-            PSP<double> psp_class(dgc);
+            PSP<double> psp_class(dgc, g, edge_map);
             psp_class.agglomerative_run();
             gamma_list = psp_class.Get_critical_values();
             psp_list = psp_class.Get_psp();
