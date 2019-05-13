ee4116038d995069ab0a72af33613934d62eb1e6
zhaofeng-shu33 <616545598@qq.com>
2019-05-14 00:49:22 +0800

    begin debug

--- core/pmf.h
+++ core/pmf.h
@@ -33,7 +33,7 @@ public:
     double compute_lambda(const std::vector<pair>& parameter_list, const double target_value);
 private:    
     void update_dig(double lambda);
-    void slice(Set& S, Set& T, const FlowMap& arcMap);
+    void slice(Set& S, Set& T, const FlowMap& arcMap, double lambda_1, double lambda_3);
 	//! modify flow given current dig_aM
 	void modify_flow(const FlowMap& flowMap, FlowMap& newFlowMap);
     inline Set get_min_cut_source_side(Preflow& pf);
