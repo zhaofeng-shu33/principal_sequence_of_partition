29d12e6d3bb3cc05bb9e4a877b1c5c53c3a1b4eb
zhaofeng-shu33 <616545598@qq.com>
2019-05-14 19:47:31 +0800

    remove some build hook for debug pmf

--- experiment/community_detection/experiment_two_level.py
+++ experiment/community_detection/experiment_two_level.py
@@ -25,7 +25,6 @@ import json
 import numpy as np
 import networkx as nx # for manipulating graph data-structure
 import graphviz # for writing .gv file
-import matplotlib.pyplot as plt
 from sklearn.metrics import adjusted_rand_score
 try:
     from ete3 import TreeStyle, NodeStyle
