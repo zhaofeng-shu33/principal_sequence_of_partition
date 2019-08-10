55e02b1948ff1b440b9d115e8410eb37c78e8191
zhaofeng-shu33 <616545598@qq.com>
2019-08-10 17:31:16 +0800

    use default preflow implementation.

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int main(int argc, const char *argv[]){
 	desc.add_options()
 		("help,h", "Show this help screen")
 		("graph", boost::program_options::value<std::string>(), "input graph file, currently only lgf format is supported")
-		("method", boost::program_options::value<std::string>()->default_value("dt"), "method to use, should be within dt, pdt, psp_i, pdt_i")
+		("method", boost::program_options::value<std::string>()->default_value("dt"), "method to use, should be within dt, pdt, psp_i, pdt_r")
 		("result", boost::program_options::value<std::string>()->default_value("output.txt"), "result file")
 		("time", boost::program_options::value<bool>()->default_value(false), "report time used");
 
