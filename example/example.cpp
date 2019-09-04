d742df071c594e9bc26d8dcdb5eee6d0f45a1c7e
zhaofeng-shu33 <616545598@qq.com>
2019-09-04 20:37:03 +0800

    compile example successful

--- example/example.cpp
+++ example/example.cpp
@@ -1,35 +1,28 @@
 #include <iostream>
+#include <tuple>
+#include <vector>
 #include <list>
-#include <lemon/list_graph.h>
 #include <psp/psp.h>
 #include <psp/set/set_stl.h>
 
 int main() {
     // construct the example graph
-    lemon::ListDigraph g;
-    lemon::ListDigraph::ArcMap<double> a(g);
-    typedef lemon::ListDigraph::Node Node;
-    Node n1 = g.add_node();
-    Node n2 = g.add_node();
-    Node n3 = g.add_node();
-    typedef lemon::ListDigraph::Arc Arc;
-    Arc a1 = g.add_arc(n1, n2);
-    a[a1] = 1;
-    Arc a2 = g.add_arc(n1, n3);
-    a[a2] = 5;
-    Arc a3 = g.add_arc(n2, n3);
-    a[a3] = 1;
+    std::vector<std::tuple<std::size_t, std::size_t, double>> arc_list;
+    arc_list.push_back(std::make_tuple(1, 2, 1.0));
+    arc_list.push_back(std::make_tuple(1, 3, 5.0));
+    arc_list.push_back(std::make_tuple(2, 3, 1.0));
     // construct the algorithm class instance
-    psp::PSP psp_instance(g, a1);
+    psp::PSP psp_instance(arc_list, 3);
     psp_instance.run();
     std::list<double> critical_values = psp_instance.get_critical_values();
-    std::list<stl::Partiton> partition_list = psp_instance.get_partitions();
+    std::list<stl::Partition> partition_list = psp_instance.get_psp();
     // output the results to the console
     std::list<double>::iterator critical_values_iterator = critical_values.begin();
-    std::list<stl::Partition> partition_list_iterator = psp_instance.begin();
+    std::list<stl::Partition>::iterator partition_list_iterator = partition_list.begin();
     for (; critical_values_iterator != critical_values.end(); critical_values_iterator++) {
         std::cout << *partition_list_iterator << std::endl;
         std::cout << *critical_values_iterator << std::endl;
+        partition_list_iterator++;
     }
     std::cout << *partition_list_iterator << std::endl;
 }
