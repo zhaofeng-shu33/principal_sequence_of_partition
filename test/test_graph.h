1248635059a195c63df0f8d7efcbfcf9fd92c473
zhaofeng-shu33 <616545598@qq.com>
2019-11-08 23:05:52 +0800

    format code

--- test/test_graph.h
+++ test/test_graph.h
@@ -11,7 +11,7 @@
 
 namespace submodular {
 
-    class SIMPLE_DGRAPH : public testing::Test {
+class SIMPLE_DGRAPH : public testing::Test {
     protected:
         lemon::ListDigraph g;
         lemon::ListDigraph::ArcMap<int> arc_map;
@@ -23,34 +23,34 @@ namespace submodular {
             arcs.push_back(std::make_tuple(1, 2, 5));
             make_dgraph(3, arcs, g, arc_map);
         }
-    };  
+};  
 
-    TEST_F(SIMPLE_DGRAPH, make_dgraph) {
-        EXPECT_EQ(g.maxNodeId(), 2);
-        EXPECT_EQ(g.maxArcId(), 2);
-        int arc_weight_count = 0;
-        for (lemon::ListDigraph::ArcIt a(g); a != lemon::INVALID; ++a)
-            arc_weight_count += arc_map[a];
-        EXPECT_EQ(arc_weight_count, 7);
-    }
+TEST_F(SIMPLE_DGRAPH, make_dgraph) {
+    EXPECT_EQ(g.maxNodeId(), 2);
+    EXPECT_EQ(g.maxArcId(), 2);
+    int arc_weight_count = 0;
+    for (lemon::ListDigraph::ArcIt a(g); a != lemon::INVALID; ++a)
+        arc_weight_count += arc_map[a];
+    EXPECT_EQ(arc_weight_count, 7);
+}
 
-    TEST_F(SIMPLE_DGRAPH, get_cut_value) {
-        stl::CSet _s;
-        EXPECT_EQ(get_cut_value(g, arc_map, _s), 0);
-        _s.AddElement(2);
-        EXPECT_EQ(get_cut_value(g, arc_map, _s), 6);
-        _s.AddElement(0);
-        EXPECT_EQ(get_cut_value(g, arc_map, _s), 5);
-        _s.AddElement(1);
-        EXPECT_EQ(get_cut_value(g, arc_map, _s), 0);
-    }
+TEST_F(SIMPLE_DGRAPH, get_cut_value) {
+    stl::CSet _s;
+    EXPECT_EQ(get_cut_value(g, arc_map, _s), 0);
+    _s.AddElement(2);
+    EXPECT_EQ(get_cut_value(g, arc_map, _s), 6);
+    _s.AddElement(0);
+    EXPECT_EQ(get_cut_value(g, arc_map, _s), 5);
+    _s.AddElement(1);
+    EXPECT_EQ(get_cut_value(g, arc_map, _s), 0);
+}
 
-    TEST_F(SIMPLE_DGRAPH, get_partition_value) {
-        EXPECT_EQ(get_partition_value(g, arc_map, stl::Partition::makeFine(3)), 7);
-        EXPECT_EQ(get_partition_value(g, arc_map, stl::Partition::makeDense(3)), 0);
-        stl::Partition _p;
-        _p.AddElement(stl::CSet("100"));
-        _p.AddElement(stl::CSet("011"));
-        EXPECT_EQ(get_partition_value(g, arc_map, _p), 2);
-    }
+TEST_F(SIMPLE_DGRAPH, get_partition_value) {
+    EXPECT_EQ(get_partition_value(g, arc_map, stl::Partition::makeFine(3)), 7);
+    EXPECT_EQ(get_partition_value(g, arc_map, stl::Partition::makeDense(3)), 0);
+    stl::Partition _p;
+    _p.AddElement(stl::CSet("100"));
+    _p.AddElement(stl::CSet("011"));
+    EXPECT_EQ(get_partition_value(g, arc_map, _p), 2);
+}
 }
